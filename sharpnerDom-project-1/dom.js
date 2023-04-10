75c0bde61b279f77c276018b074afcad43e86efa
dennis <dennisjshofficial@gmail.com>
2023-04-10 08:51:25 +0530

    project1 has been added

---
+++ sharpnerDom-project-1/dom.js
@@ -0,0 +1,6 @@
+var header=document.getElementById("main-header");
+header.style.borderBottom = "solid 3px black"  // this will give the border for the header.
+
+var fontChange=document.getElementById("main");
+fontChange.style.fontWeight="bold";
+fontChange.style.color="green";
