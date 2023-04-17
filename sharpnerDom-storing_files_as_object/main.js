1c47860bbfaa2a5acf54617739e29c38036a16cd
dennis <dennisjshofficial@gmail.com>
2023-04-18 00:02:39 +0530

    storing objects in local storage has been added

---
+++ sharpnerDom-storing_files_as_object/main.js
@@ -0,0 +1,54 @@
+// USER FORM SCRIPT.
+
+// PUT DOM ELEMENTS INTO VARIABLES:
+const myForm = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const phoneInput = document.querySelector("#phone");
+const dateInput = document.querySelector("#date");
+const msg = document.querySelector(".msg");
+
+
+// LISTEN FOR SUBMIT
+myForm.addEventListener("submit",onSubmit);
+
+function onSubmit(e)
+{
+    e.preventDefault();
+
+    if(nameInput.value === "" || emailInput.value ==="")
+    {
+        // alert("please enter all the fields")
+        msg.classList.add("error");
+        msg.innerHTML = "Please enter both Name and Email";
+    }
+    else{
+        // store item in the local storage.
+        // get the target values first.
+        let myObj={
+        name : e.target.name.value,
+        email : e.target.email.value,
+        phone : e.target.phone.value,
+        date : e.target.date.value
+        }
+
+        // convert the object into a string.
+        let myObj_serialized = JSON.stringify(myObj);
+        
+        // check if working by console logging
+        console.log(myObj_serialized);
+        
+        // saving the string in the local storage.
+        localStorage.setItem("userDetails",myObj_serialized);
+
+        // retreving from the local storage.
+        const myObj_deserialized = JSON.parse(localStorage.getItem("userDetails"));
+
+        // clear fields
+        nameInput.value="";
+        emailInput.value="";
+        phoneInput.value="";
+        dateInput.value="";
+
+    }
+}
